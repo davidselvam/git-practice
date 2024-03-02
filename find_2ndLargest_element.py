ebfd0f825883e08f4b8b821e31428a7d8d8c9c1f
davidselvam <smilingdavid001@gmail.com>
2024-03-02 13:34:22 +0530

    update : rewrite the second largest element

--- find_2ndLargest_element.py
+++ find_2ndLargest_element.py
@@ -1,9 +1,7 @@
-# Find the second largest element in the list
-
 def second_largest_element(x):
     x.sort()
     return x[-2]
 
-marks=[446,334,55,666,77,88,99]
+marks=[44,33,55,66,77,88,99]
 a = second_largest_element(marks)
 print(a)
