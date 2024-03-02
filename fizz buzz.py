e960e2ff4d354f4394cb0014303679df7183baf3
davidselvam <smilingdavid001@gmail.com>
2024-03-02 18:44:33 +0530

    added : Find the fizz buzz

---
+++ fizz buzz.py
@@ -0,0 +1,12 @@
+# find the fizz buzz
+
+n=int(input("Enter a range of number :"))
+for i in range(1,n+1):
+    if i%3 == 0 and i%5 == 0:
+        print("Fizz Buzz !!!")
+    elif i%3==0:
+        print("Fizz...")
+    elif i%5==0:
+        print("Buzz...")
+    else:
+        print(i)
